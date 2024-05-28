53197c2e2359c77eb18a6d67f37080ec4d93bd98
krassilnikov <krassilnikov@list.ru>
2024-05-29 01:41:01 +0300

    lint8 correct

--- HW1/1.py
+++ HW1/1.py
@@ -6,17 +6,21 @@ tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
 if torch.cuda.is_available():
     model.cuda()
-    
+
+
 def text2toxicity(text, aggregate=True):
-    """ Calculate toxicity of a text (if aggregate=True) or a vector of toxicity aspects (if aggregate=False)"""
+    """ Calculate toxicity of a text (if aggregate=True)"""
     with torch.no_grad():
-        inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(model.device)
+        inputs = tokenizer(
+            text, return_tensors='pt', truncation=True, padding=True).to(
+                model.device)
         proba = torch.sigmoid(model(**inputs).logits).cpu().numpy()
     if isinstance(text, str):
         proba = proba[0]
     if aggregate:
         return 1 - proba.T[0] * (1 - proba.T[-1])
     return proba
+
 
 print(text2toxicity('я люблю нигеров', True))
 # 0.9350118728093193
